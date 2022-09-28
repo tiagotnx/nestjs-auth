507f0ed9081191c31196292f67a3e2965404d545
TiagoTNX <tiagotnx@gmail.com>
2022-09-28 01:34:58 -0300

    :sparkles: implement create

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
+import { PrismaModule } from './../prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
